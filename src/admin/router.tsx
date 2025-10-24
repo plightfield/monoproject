c032f18ee6c24f10eebd135921302f3ba550fda9
plightfield <120712.484@qq.com>
2025-10-24 14:22:20 +0800

    change

--- src/admin/router.tsx
+++ src/admin/router.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react"
 import { createBrowserRouter, type RouteObject } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 
-// 演示路由使用
+// 演示路由使用，路由单独提出是为了有 menu 功能时好生成菜单
 export const routes: (RouteObject & { icon: ReactNode; label: string })[] = [
   {
     path: "/",
